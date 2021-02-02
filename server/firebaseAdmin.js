53e257ff31a26549c7d35ff15c5010b67fdabbc2
Leo Zhang <balabagi2012@gmail.com>
2021-02-02 15:22:21 +0800

    feat(Server):init project
    a basic express framework with firebaseAdmin

---
+++ server/firebaseAdmin.js
@@ -0,0 +1,8 @@
+import admin from "firebase-admin";
+var serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
+
+const adminApp = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export default admin;
