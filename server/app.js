5980a3b5cc90d38443b16f6eb699f42ca89a8643
Leo Zhang <balabagi2012@gmail.com>
2021-02-02 17:52:50 +0800

    feat(MessageQueue):init RabbitMQ

--- server/app.js
+++ server/app.js
@@ -1,23 +1,30 @@
 import dotenv from "dotenv";
 dotenv.config();
+import RabbitMQ from "./rabbitMQ";
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
+RabbitMQ.initSockets();
 
-app.use(logger('dev'));
+RabbitMQ.subFCM.on("data", function (note) {
+  console.log("Alarum! %s", JSON.parse(note));
+});
+setTimeout(() => {
+  RabbitMQ.pubFCM.write(JSON.stringify({ a: 1 }), "utf8");
+}, 1000);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
---
+++ server/rabbitMQ.js
@@ -0,0 +1,29 @@
+import rabbit from "rabbit.js";
+
+class RabbitMQ {
+  static context = null;
+  static pubFCM;
+  static subFCM;
+  static pubDone;
+  static subDone;
+
+  static async initSockets() {
+    if (this.context === null) {
+      this.context = rabbit.createContext("amqp://leo:leo_password@localhost:5672");
+      this.pubFCM = this.context.socket("PUBLISH");
+      this.subFCM = this.context.socket("SUBSCRIBE");
+      this.pubFCM.connect("notification.fcm");
+      this.subFCM.connect("notification.fcm");
+      // this.pubFCM.setEncoding("utf8");
+      this.subFCM.setEncoding("utf8");
+      this.pubDone = this.context.socket("PUBLISH");
+      this.subDone = this.context.socket("SUBSCRIBE");
+      this.pubDone.connect("notification.done");
+      this.subDone.connect("notification.done");
+      // this.pubDone.setEncoding("utf8");
+      this.subDone.setEncoding("utf8");
+    }
+  }
+}
+
+export default RabbitMQ;
